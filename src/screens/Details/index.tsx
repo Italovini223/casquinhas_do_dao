421ae0ec51932cd676ec89df84fe15230510ecc0
Italovini223 <italovinicius2018@gmail.com>
2024-02-13 14:15:41 -0300

    ✨ feat: getting order details

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -2,11 +2,18 @@ import { useCallback  } from 'react'
 
 import { useFocusEffect  } from '@react-navigation/native'
 
+import { useObject } from '../../libs/realm'
+
+import { BSON } from 'realm'
+
+
 import { useRoute } from '@react-navigation/native'
 
-import { Container } from './styles'
+import { Container, Content, Status, Label, Price, Product, Quantity } from './styles'
 
 import { Header } from '../../components/Header';
+import { Order } from '../../libs/realm/schemas/order'
+
 
 type RouteParams = {
   id: string;
@@ -16,13 +23,49 @@ export function Details() {
   const routes = useRoute();
   const { id } = routes.params as RouteParams;
 
-  useFocusEffect(useCallback(() => {
-    console.log(id);
-  }, [id]));
-  
+  const order = useObject(Order, new BSON.UUID(id) as unknown as string);
+
+  const orderStatus = order?.order_status === 'in preparation' ? 'Em preparação' : 'Pedido Pronto'
+  const paymentStatus = order?.its_paid == true ? 'Pago' : 'Pendente pagamento';
+
   return (
     <Container>
       <Header title='Detalhes'/>
+      
+      <Content>
+        <Label> Produto </Label>
+        <Product>
+          { order?.product_name }
+        </Product>
+
+        <Label>
+          Quantidade p-10
+        </Label>
+        <Quantity>
+          { order?.product_quantity }
+        </Quantity>
+
+        <Label>
+          Preço total
+        </Label>
+        <Price>
+          R$ { order?.total_price },00
+        </Price>
+
+        <Label>
+          Status do pedido
+        </Label>
+        <Status>
+          { orderStatus }
+        </Status>
+
+        <Label>
+          Status do pagamento
+        </Label>
+        <Status>
+          { paymentStatus }
+        </Status>
+      </Content>
     </Container>
   );
 }
--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -5,3 +5,42 @@ export const Container = styled.View`
 
   background-color: ${({ theme }) => theme.COLORS.GRAY_800};
 `;
+
+export const Content = styled.View`
+  flex: 1;
+  
+  padding: 0 30px;
+`;
+
+export const Label = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+
+  margin-top: 32px;
+  margin-bottom: 5px;
+`;
+
+export const Product = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XXL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`;
+
+export const Quantity = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR}
+`;
+
+export const Price = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD}
+`;
+
+export const Status = styled.Text`
+color: ${({ theme }) => theme.COLORS.GRAY_200};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR}
+`;
